6b32fda28eebbfb3263ef16d8ba64b2a9830e02e
Vinicius Oliveira <viniciusp.oliveira@icloud.com>
2024-12-27 08:10:03 -0300

    first commit class appium

---
+++ src/main/java/lavoscracker/appiumaula/App.java
@@ -0,0 +1,10 @@
+package lavoscracker.appiumaula;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/lavoscracker/appiumaula/CoreJavaBrushup1.java
@@ -0,0 +1,20 @@
+package lavoscracker.appiumaula;
+
+public class CoreJavaBrushup1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int myNum = 5;
+		String websit = "Vinicius Oliveira";
+		char letter = 'r';
+		
+		
+		System.out.println(myNum + "é o numero que preciso imprimir");
+		
+		
+
+	}
+
+}
