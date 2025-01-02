7adf2ceb7b7ba889bba228612ca3043817589b5e
Vinicius Oliveira <viniciusp.oliveira@icloud.com>
2025-01-02 08:19:26 -0300

    fixing code

--- src/test/java/lavoscracker/appiumaula/AppiumBasics.java
+++ src/test/java/lavoscracker/appiumaula/AppiumBasics.java
@@ -2,6 +2,7 @@ package lavoscracker.appiumaula;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.testng.annotations.Test;
 
@@ -9,15 +10,19 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class AppiumBasics {
+
+
 	@Test
-	public void AppiumTest() throws MalformedURLException
+	public void AppiumTest() throws MalformedURLException, URISyntaxException
 	{
 		
 		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName(null)
+		options.setDeviceName("PixelProduction");
+		options.setApp("//Users//viniciusoliveira//eclipse-workspace//appiumaula//src//test//java//resources//ApiDemos-debug.apk");
 		
 		
-	AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), null);
+	AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+	driver.quit();
 	
 	//Actual Automation
 		
